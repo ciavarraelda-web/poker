60137a0359b1056acb8a867051a792274703eabb
ciavarraelda-web <ciavarraelda@gmail.com>
2025-08-26 15:08:50 +0200

    Create config.ts

---
+++ backend/src/config.ts
@@ -0,0 +1,4 @@
+export const FEE_WALLET = "DZoHMBRyTzShZC9dwQ2HgFwhSjUE2xWLEDypKoa2Mcp3";
+export const TABLE_FEE_PERCENT = 3; // % fee a mano
+export const SUBSCRIPTION_COST_SOL = 0.05; // costo abbonamento in SOL
+export const SOLANA_RPC = "https://api.mainnet-beta.solana.com";
